238eeaab8e95a1736ee2b1696a305ab2d7d90b5d
goshaGloss <georgiy.chetkov@gmail.com>
2025-05-04 11:58:26 +0500

    feat:games

--- src/app/(main)/main/components/mini-games/mini-games.tsx
+++ src/app/(main)/main/components/mini-games/mini-games.tsx
@@ -13,60 +13,59 @@ const MiniGames = () => {
             <Image
               src="/main-game.png"
               alt="main-game"
-              width={140}
-              height={140}
+              width={225}
+              height={200}
             />
             <p className={styles.playNow}>ИГРАТЬ СЕЙЧАС</p>
           </div>
-
-          <div className={`${styles.card} ${styles.orange}`}>
-            <div className={styles.content}>
-              <p>Угадай артиста</p>
-              <Image
-                src="/guess-note.png"
-                alt="guess-note"
-                width={100}
-                height={100}
-              />
-            </div>
-          </div>
-
-          <div className={`${styles.card} ${styles.blue}`}>
-            <div className={styles.content}>
-              <p>Ритм-мастер</p>
-              <div style={{ display: "flex", gap: "10px" }}>
+          <div className={styles.cardColumn}>
+            <div className={`${styles.card} ${styles.orange}`}>
+              <div className={styles.content}>
+                <p>Угадай артиста</p>
                 <Image
-                  src="/woman-guitar.png"
-                  alt="woman"
-                  width={60}
-                  height={90}
+                  src="/guess-note.png"
+                  alt="guess-note"
+                  width={156}
+                  height={156}
                 />
+              </div>
+            </div>
+            <div className={`${styles.card} ${styles.pink}`}>
+              <div className={styles.content}>
+                <p>Музыкальная викторина</p>
                 <Image
-                  src="/rhythm-master.png"
-                  alt="note"
-                  width={30}
-                  height={50}
+                  src="/musical.png"
+                  alt="musical"
+                  width={50}
+                  height={40}
                 />
               </div>
             </div>
           </div>
-
-          <div className={`${styles.card} ${styles.pink}`}>
-            <div className={styles.content}>
-              <p>Музыкальная викторина</p>
-              <Image src="/musical.png" alt="musical" width={50} height={40} />
+          <div className={styles.cardColumn}>
+            <div className={`${styles.card} ${styles.blue}`}>
+              <div className={styles.content}>
+                <p>Ритм-мастер</p>
+                <div style={{ display: "flex", gap: "10px" }}>
+                  <Image
+                    src="/woman-guitar.png"
+                    alt="woman"
+                    width={120}
+                    height={160}
+                  />
+                </div>
+              </div>
             </div>
-          </div>
-
-          <div className={`${styles.card} ${styles.orange}`}>
-            <div className={styles.content}>
-              <p>ИГРАТЬ ЕЩЕ</p>
-              <Image
-                src="/play-more.png"
-                alt="play-more"
-                width={80}
-                height={80}
-              />
+            <div className={`${styles.card} ${styles.orange}`}>
+              <div className={styles.content}>
+                <p>ИГРАТЬ ЕЩЕ</p>
+                <Image
+                  src="/play-more.png"
+                  alt="play-more"
+                  width={125}
+                  height={120}
+                />
+              </div>
             </div>
           </div>
         </div>
